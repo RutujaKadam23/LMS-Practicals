fc7672caff1781f0309f3077fa2c7eb20d864daf
Rutuja Kadam <128777089+RutujaKadam23@users.noreply.github.com>
2023-04-01 22:59:28 +0530

    Add files via upload

---
+++ Core Java/Chapter 1/DemoPrimitiveDatatypes.java
@@ -0,0 +1,20 @@
+//WRITE A JAVA PROGRAM TO DEMO ABOUT ALL JAVA PRIMITIVE DATATYPES?
+
+class DemoPrimitiveDatatypes
+{
+	public static void main(String args[])
+	{
+		System.out.println("-----Primitive DataTypes-----");
+		byte b=127;
+		short s=32767;
+		int i=2147483647;
+		long l=9223372036854775807L;
+		float f=1233.7f;
+		double d=234345654676545d;
+		boolean b1=true;
+		char ch='R';
+		
+		System.out.println("Byte: "+b+"\nShort: "+s+"\nInt: "+i+"\nLong: "+l+"\nFloat: "+f+"\nDouble: "+d+"\nBoolean: "+b1+"\nCharacter: "+ch);
+		
+	}
+}
